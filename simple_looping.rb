8c306c79681539c031421ca6db15be6c47ae2c79
Bryce Eadie <bryce.eadie@gmail.com>
2018-09-27 01:41:14 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -40,9 +40,12 @@ end
 end
 
 def for_iterator(number_of_times)
-  
-  
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  for counter in 1..number_of_times
+  puts phrase
+end
+  
+  
   
 end
 
